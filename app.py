8f112968bc6ab49b13ac90b1ffc7ee233c5720a0
aliajproject <ali.aj.project@gmail.com>
2025-10-28 17:21:39 +0400

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def get_messages():
 
 if __name__ == "__main__":
     print("🛍️ Shopping Chat server işə salındı: http://127.0.0.1:5000")
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
